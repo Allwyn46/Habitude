cb5c60b8125c1687ab0a40f250f35358a9313aaa
Allwyn46 <allwyn153@gmail.com>
2025-10-24 19:19:04 +0530

    added todo functions in habit service

--- src/app/services/habitservice.ts
+++ src/app/services/habitservice.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 import { toast } from 'ngx-sonner';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Habitservice {
+  http = inject(HttpClient);
+
   showSuccessToast(title: string, desc: string) {
     toast.success(title, {
       description: desc,
@@ -15,5 +18,25 @@ export class Habitservice {
     toast.error(title, {
       description: desc,
     });
+  }
+
+  addtodo(formdata: any) {
+    return this.http.post('/api/todo/create', formdata);
+  }
+
+  getalltodo(userid: any) {
+    return this.http.post('/api/todo/getall', userid);
+  }
+
+  updateTodo(formdata: any) {
+    return this.http.post('/api/todo/update', formdata);
+  }
+
+  deleteTodo(id: any) {
+    return this.http.post('/api/todo/delete', id);
+  }
+
+  markComplete(id: any) {
+    return this.http.post('/api/todo/mark-as-complete', id);
   }
 }
