09596008f0e7983252c83c2e5f1c56e3028238a8
Allwyn46 <allwyn153@gmail.com>
2025-11-03 18:46:29 +0530

    Minor changes in add and delete button positions in todo-card component

--- src/app/components/todo-card/todo-card.ts
+++ src/app/components/todo-card/todo-card.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { ZardAlertComponent } from 'n/alert/alert.component';
+import { ZardButtonComponent } from 'n/button/button.component';
+import { ZardIconComponent } from 'n/icon/icon.component';
 
 @Component({
   selector: 'app-todo-card',
-  imports: [ZardAlertComponent],
+  imports: [ZardAlertComponent, ZardButtonComponent, ZardIconComponent],
   templateUrl: './todo-card.html',
   styleUrl: './todo-card.css',
 })
