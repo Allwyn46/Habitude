b4b7cf3297572d4f25c2bb22427041a134c4dd8d
Allwyn46 <allwyn153@gmail.com>
2025-11-04 17:51:29 +0530

    Replaced the edit button and implemented the open sheet fuction

--- src/app/components/edit-todo/edit-todo.ts
+++ src/app/components/edit-todo/edit-todo.ts
@@ -1,5 +1,6 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ZardButtonComponent } from 'n/button/button.component';
 import { ZardSheetModule } from 'n/sheet/sheet.module';
 import { ZardSheetService } from 'n/sheet/sheet.service';
@@ -10,7 +11,7 @@ import { Habitservice } from 'src/app/services/habitservice';
 
 @Component({
   selector: 'app-edit-todo',
-  imports: [],
+  imports: [AsyncPipe, ReactiveFormsModule],
   templateUrl: './edit-todo.html',
   styleUrl: './edit-todo.css',
 })
@@ -59,7 +60,27 @@ export class EditTodo implements OnInit {
   selector: 'app-edit-todo-trigger',
   standalone: true,
   imports: [ZardButtonComponent, ZardSheetModule],
-  template: ` <button z-button (click)="openSheet()">Add Todo</button> `,
+  template: `
+    <button z-button zType="ghost" (click)="openSheet()" class="absolute top-1 right-28 z-50">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-pencil-icon lucide-pencil"
+      >
+        <path
+          d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"
+        />
+        <path d="m15 5 4 4" />
+      </svg>
+    </button>
+  `,
 })
 export class EditTodoTrigger {
   private sheetService = inject(ZardSheetService);
--- src/app/components/todo-card/todo-card.ts
+++ src/app/components/todo-card/todo-card.ts
@@ -2,14 +2,16 @@ import { Component, Input } from '@angular/core';
 import { ZardAlertComponent } from 'n/alert/alert.component';
 import { ZardButtonComponent } from 'n/button/button.component';
 import { ZardIconComponent } from 'n/icon/icon.component';
+import { EditTodoTrigger } from '../edit-todo/edit-todo';
 
 @Component({
   selector: 'app-todo-card',
-  imports: [ZardAlertComponent, ZardButtonComponent, ZardIconComponent],
+  imports: [ZardAlertComponent, ZardButtonComponent, ZardIconComponent, EditTodoTrigger],
   templateUrl: './todo-card.html',
   styleUrl: './todo-card.css',
 })
 export class TodoCard {
   @Input() title!: string;
   @Input() description!: string;
+  @Input() todoid!: string;
 }
